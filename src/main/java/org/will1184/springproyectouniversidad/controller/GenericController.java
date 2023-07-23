5dd9aa11c734bd59b318341cd60934845e632e0a
Will1184 <brandonwilliammg@gmail.com>
2023-07-23 14:54:46 -0600

    Feat: se agregaron nuevos metodos de consulta en GenericController

--- src/main/java/org/will1184/springproyectouniversidad/controller/GenericController.java
+++ src/main/java/org/will1184/springproyectouniversidad/controller/GenericController.java
@@ -1,10 +1,13 @@
 package org.will1184.springproyectouniversidad.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.will1184.springproyectouniversidad.exception.BadRequestException;
+import org.will1184.springproyectouniversidad.model.entity.Carrera;
+import org.will1184.springproyectouniversidad.model.entity.Persona;
 import org.will1184.springproyectouniversidad.service.contratos.GenericoDAO;
 
 import java.util.List;
+import java.util.Optional;
 
 public class GenericController<E,S extends GenericoDAO<E>> {
     protected final S service;
@@ -20,5 +23,28 @@ public class GenericController<E,S extends GenericoDAO<E>> {
             throw new BadRequestException(String.format("No se han encontrado %ss",nombreEntidad));
         }
         return list;
+    }
+
+    @PostMapping
+    public E altaEntidad(@RequestBody E entidad){
+        return service.save(entidad);
+    }
+
+    @GetMapping("/{id}")
+    public E obtenerPorId(@PathVariable(required = false) Integer id){
+        Optional<E> oEntidad = service.findById(id);
+        if(!oEntidad.isPresent()) {
+            throw new BadRequestException(String.format("Entidad con id %d no existe", id));
+        }
+        return oEntidad.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarPorId(@PathVariable Integer id){
+        Optional<E> oEntidad = service.findById(id);
+        if(!oEntidad.isPresent()) {
+            throw new BadRequestException(String.format("Entidad con id %d no existe", id));
+        }
+        service.deleteById(id);
     }
 }
